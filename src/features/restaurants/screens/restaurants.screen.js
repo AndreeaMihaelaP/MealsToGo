ca1ed4f99110bbad32096ff766d5bbe99f4e79d4
AndreeaMihaelaP <andreeapatrascu1997@gmail.com>
2021-10-14 19:42:43 +0300

    Added restaurant screen

---
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import { Searchbar } from "react-native-paper";
+import { StatusBar, StyleSheet, SafeAreaView, View } from "react-native";
+import { RestaurantInfo } from "../components/restaurant-info.component";
+
+const { currentHeight } = StatusBar;
+
+export const RestaurantsScreen = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const onChangeSearch = (query) => setSearchQuery(query);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.search}>
+        <Searchbar
+          placeholder="Search"
+          onChangeText={onChangeSearch}
+          value={searchQuery}
+        />
+      </View>
+      <View style={styles.list}>
+        <RestaurantInfo />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: currentHeight,
+  },
+  search: {
+    padding: 16,
+  },
+  list: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: "blue",
+  },
+});
